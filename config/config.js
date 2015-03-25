12deb30e8ffb64728118d885aedcef7fa2a939b1
jesperkm <jesperkmartinussen@gmail.com>
2015-03-25 14:36:16 +0100

    fixing webhooks list style, and removing unnecessary styles and comments

--- config/config.js
+++ config/config.js
@@ -7,8 +7,8 @@
 var config = {};
 
 /* Trello API Access */
-config.trelloApplicationKey = "<<Insert your application key>>"; // Read https://trello.com/docs/gettingstarted/index.html#getting-an-application-key
-config.trelloUserToken = "<<Insert your user token>>"; // Read https://trello.com/docs/gettingstarted/index.html#getting-a-token-from-a-user
+config.trelloApplicationKey = "b7a8480d496fa90c36f9f2d02020e25b"; // Read https://trello.com/docs/gettingstarted/index.html#getting-an-application-key
+config.trelloUserToken = "0bf31a4ea7b08e514839f76a0dda3c5bcb6f133a6a85e6954d4bdb9b03eabc88"; // Read https://trello.com/docs/gettingstarted/index.html#getting-a-token-from-a-user
 
 /* Server port of Webhooks for Trello */
 config.serverport = 3001;
